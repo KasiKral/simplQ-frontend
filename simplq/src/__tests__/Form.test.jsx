e0a96b795f3b7c76efa98ea1ea5e72d136b29585
Adam Kasela <adam.kasela@student.tuke.sk>
2020-11-17 17:53:20 +0100

    preccommit testing

--- simplq/src/__tests__/Form.test.jsx
+++ simplq/src/__tests__/Form.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
@@ -6,31 +7,28 @@ import Form from '../components/pages/Join/Form';
 afterEach(cleanup);
 
 it('renders correctly', () => {
-    const { asFragment } = render(<Form />);
-    expect(asFragment()).toMatchSnapshot();
+  const { asFragment } = render(<Form />);
+  expect(asFragment()).toMatchSnapshot();
 });
 
-describe("<Form>", () => {
-    it("Name placholder change", () => {
-        const { getByPlaceholderText } = render(<Form />);
+describe('<Form>', () => {
+  it('Name placholder change', () => {
+    const { getByPlaceholderText } = render(<Form />);
 
-        const placeholderText = 'Name';
-        let userInput = "Adam";
-        fireEvent.change(getByPlaceholderText(placeholderText), { target: { value: userInput } });
-        expect(getByPlaceholderText(placeholderText)).toHaveValue(userInput);
-    });
+    const placeholderText = 'Name';
+    let userInput = 'Adam';
+    fireEvent.change(getByPlaceholderText(placeholderText), { target: { value: userInput } });
+    expect(getByPlaceholderText(placeholderText)).toHaveValue(userInput);
+  });
 
-    it("Number placholder change", () => {
-        const { getByPlaceholderText } = render(<Form />);
+  it('Number placholder change', () => {
+    const { getByPlaceholderText, getByRole } = render(<Form />);
+    const prefix = getByRole('button', { name: /india: \+ 91/i });
 
-        const placeholderText = 'Phone Number';
-        let userInput = "8555214486";
-        fireEvent.change(getByPlaceholderText(placeholderText), { target: { value: userInput } });
-        expect(getByPlaceholderText(placeholderText)).toHaveValue(`+${"855 521 448 6"}`);
-    });
-
-    it("Contains submit button", () => {
-        const { getByText, container } = render(<Form />);
-        fireEvent.click(getByText('Join Queue'));
-    });
+    const placeholderText = 'Phone Number';
+    let userInput = '8555214486';
+    fireEvent.change(getByPlaceholderText(placeholderText), { target: { value: userInput } });
+    expect(getByPlaceholderText(placeholderText)).toHaveValue(`+${'855 521 448 6'}`);
+    expect(prefix).toBeInTheDocument();
+  });
 });
